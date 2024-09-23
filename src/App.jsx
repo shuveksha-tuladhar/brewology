3f99e325b7f64d76819ed25323a2ed54bc66c669
Shuveksha Tuladhar <tuladhar.shuveksha@gmail.com>
2024-09-23 14:51:47 -0700

    feat(#1): Initial project setup, added pages, favicons and routes for the pages.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HotDrinks } from "./pages/HotDrinks";
+import { IcedDrinks } from "./pages/IcedDrinks";
+import { Home } from './pages/Home';
+
+export function App() {
+  return (
+    <Router>
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path="/hot-drinks" element={<HotDrinks />} />
+          <Route path="/iced-drinks" element={<IcedDrinks />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/pages/IcedDrinks.jsx
@@ -0,0 +1,7 @@
+export function IcedDrinks() {
+    return (
+        <div>
+            This is Iced Drinks Page. 
+        </div>
+    )
+}
